75843b1d65e90665c21819a0e49b2255f4a2e028
MarinaLik <mylym79@gmail.com>
2017-05-14 18:15:54 +0300

    Доработан выезд формы и отключение с помощью esc.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 var link = document.querySelector(".modal-btn");
 var popup = document.querySelector(".modal-form");
-/*var arrival-date = popup.querySelector("[name=arrival-date]");
-var departure-date = popup.querySelector("[name=departure-date]");
-var adult = popup.querySelector("[name=adult]");*/
 
+popup.classList.remove("modal-form-visible");
+popup.classList.add("modal-form-invisible");
 
 link.addEventListener("click", function(event) {
     event.preventDefault();
@@ -11,17 +10,9 @@ link.addEventListener("click", function(event) {
     popup.classList.toggle("modal-form-visible");
 });
 
-/*popup.addEventListener("submit", function(event) {
-    if (!arrival-date.value || !departure-date.value || !adult.value) {
-        event.preventDefault();
-        console.log("Введите, пожалуйста, дату заезда, дату выезда и количество взрослых");
-    }
-});*/
-
 window.addEventListener("keydown", function(event) {
-    if (event.keyCode === 27) {
-        if (popup.classList.contains("modal-form-visible")) {
-            popup.classList.remove("modal-form-visible");
-        }
+    if (event.keyCode === 27 && popup.classList.contains("modal-form-visible")) {
+        popup.classList.remove("modal-form-visible");
+        popup.classList.add("modal-form-invisible");
     }
 });
